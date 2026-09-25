296e3ec1ac1d96039dbcb501576acec8507c7959
Nikita Garamov <garamovn@mail.ru>
2026-09-25 03:10:12 -0700

    Добавлено приветствие и обработка команд

---
+++ src/primitive_db/engine.py
@@ -0,0 +1,19 @@
+import prompt
+
+def welcome():
+    print("Первая попытка запустить проект!")
+    print()
+    print("***")
+    print("<command> exit - выйти из программы")
+    print("<command> help - справочная информация")
+
+    while True:
+        command = prompt.string("Введите команду: ")
+
+        if command == "exit":
+            break
+
+        if command == "help":
+            print()
+            print("<command> exit - выйти из программы")
+            print("<command> help - справочная информация")
